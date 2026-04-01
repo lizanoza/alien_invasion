f64782f1adac34f3ccf1f2968171720588ad1a24
Norman Lizano <normanlizaza@gmail.com>
2026-04-01 16:12:56 -0600

    settings_eje module created

---
+++ settings_eje.py
@@ -0,0 +1,16 @@
+
+class Settings2:
+    def __init__(self):
+        """Initialize game settings"""
+        self.screen_width = 1520
+        self.screen_height = 760
+        self.bg_color = (0, 0, 250) # En hexadecimal #0000FA
+        #Ship settings
+        self.ship_speed = 1.5
+        #Bullet settings
+        self.bullet_speed = 2
+        self.bullet_height = 3
+        self.bullet_width = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 5
+
