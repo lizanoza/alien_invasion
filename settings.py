1fd16d908786bad49891e4750f7a5149a8d1b2b7
Norman Lizano <normanlizaza@gmail.com>
2026-04-01 16:02:41 -0600

    bullet settings updated

--- settings.py
+++ settings.py
@@ -5,5 +5,15 @@ class Settings:
         """Initialize the game´s settings"""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 760
         self.background_color = (230 , 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
