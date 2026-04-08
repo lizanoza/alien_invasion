f6b56185bd59c34d78a6ff2b6278fe3ccb94a1cd
Norman Lizano <normanlizaza@gmail.com>
2026-04-08 15:46:37 -0600

    alien settings updated

--- settings_eje.py
+++ settings_eje.py
@@ -13,4 +13,8 @@ class Settings2:
         self.bullet_width = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
+        #Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_left_speed = 10
+        self.fleet_direction = 1
 
