a7a7647d5003a6c85fc95b5588e37ff2c50606a5
Norman Lizano <normanlizaza@gmail.com>
2026-04-27 16:54:52 -0600

    Initialize dynamic settings, increase speed and set difficulty methods created in settings

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 class Settings:
     """A class to store all settings from Alien Invasion"""
     def __init__(self):
-        """Initialize the game´s settings"""
+        """Initialize the game´s static settings"""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 760
@@ -21,6 +21,49 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 50
+        self.fleet_drop_speed = 10
         # Fleet direction of 1 represents right, -1 represents left
         self.fleet_direction = 1
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.5
+        self.difficulty_level = 'medium'
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        if self.difficulty_level == 'easy':
+            self.ship_limit = 5
+            self.bullets_allowed = 10
+            self.ship_speed = 0.75
+            self.bullet_speed = 1.5
+            self.alien_speed = 0.5
+        elif self.difficulty_level == 'medium':
+            self.ship_limit = 3
+            self.bullets_allowed = 3
+            self.ship_speed = 1.5
+            self.bullet_speed = 3.0
+            self.alien_speed = 1.0
+        elif self.difficulty_level == 'difficult':
+            self.ship_limit = 2
+            self.bullets_allowed = 3
+            self.ship_speed = 3.0
+            self.bullet_speed = 6.0
+            self.alien_speed = 2.0
+
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+    def set_difficulty(self, diff_setting):
+        if diff_setting == 'easy':
+            print('easy')
+        elif diff_setting == 'medium':
+            pass
+        elif diff_setting == 'difficult':
+            pass
